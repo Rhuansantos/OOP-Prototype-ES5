5f57ceb68c915fd0ade14defc9a118b88f09b16d
Rhuan Souza <rhuansantosdev@gmail.com>
2017-06-08 14:16:05 -0400

    update

--- js/classes.js
+++ js/classes.js
@@ -1,25 +1,23 @@
-// ==== ES5
+// ================== ES5 ================== //
 var Shelter = (function (_id, _location){
 	function Shelter(){
 		this.id = _id;
 		this.location = _location;
 	}
-	
 	return Shelter;
 });
 
 var Pet = (function (_name, _age){
-	console.log(_name);
+	Pet.prototype = Object.create(Shelter.prototype);
 	function Pet(){
+		Shelter.call(this);
 		this.name = _name;
 		this.age = _age;
-		Pet.prototype.create();
+		Pet.prototype.create.call(this);
+		console.log('2');
 	}
-
 	 Pet.prototype.create = function (){
-
         console.log('I was created');
     };
-
 	return Pet;
 });
