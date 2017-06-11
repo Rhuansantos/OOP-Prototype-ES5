2eff27d3b391b5c69bdcc4fa945f5521c5aaec5e
Rhuan Souza <rhuansantosdev@gmail.com>
2017-06-11 13:36:05 -0400

    Classes working with inherence

--- js/classes.js
+++ js/classes.js
@@ -1,23 +1,51 @@
+var petArray = [];
+var petContainer = document.getElementById('petList');
+
 // ================== ES5 ================== //
-var Shelter = (function (_id, _location){
-	function Shelter(){
+var Shelter = (function (){
+	function Shelter(_id, _location){
 		this.id = _id;
-		this.location = _location;
+		this.location = 'test';
 	}
 	return Shelter;
 });
 
-var Pet = (function (_name, _age){
+var Pet = (function (){
 	Pet.prototype = Object.create(Shelter.prototype);
-	function Pet(){
+	function Pet(_name, _age, _type){
 		Shelter.call(this);
 		this.name = _name;
 		this.age = _age;
-		Pet.prototype.create.call(this);
-		console.log('2');
+		this.type = _type;
+		Pet.prototype.buildObj.call(this);
 	}
-	 Pet.prototype.create = function (){
-        console.log('I was created');
-    };
+
+	 Pet.prototype.buildObj = function() {
+		 petObj = {};
+		 petObj.name = this.name;
+		 petObj.age = this.age;
+		 petObj.type = this.type;
+
+	   petArray.push(petObj);
+
+		 Pet.prototype.display.call(this);
+	 };
+	 Pet.prototype.display = function (){
+
+		 petArray.forEach(function(el, i) {
+			 var template = `
+				 <li data-key=${i}>
+						<h4>Name: ${el.name}</h4>
+						<h4>Age: ${el.age}</h4>
+						<h4>Type: ${el.petType}</h4>
+						<h4>Location: ${el.shelter}</h4>
+						 <hr />
+				 </li>
+			 `;
+			 petContainer.insertAdjacentHTML('beforeend', template);
+
+		 });
+  };
+
 	return Pet;
-});
+})();
--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 window.addEventListener('load', function(){
 
 // Form
-document.getElementById('insertPet').addEventListener('submit', 
+document.getElementById('insertPet').addEventListener('submit',
 	function(e){
 		e.preventDefault();
 
 		var petName = document.getElementById('name').value;
 		var petAge = document.getElementById('age').value;
+
 
 
 		var newPet = Pet(petName, petAge);
